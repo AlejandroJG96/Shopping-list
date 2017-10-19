2dc866921c94177e5db8c0590692640d67411d4c
AlejandroJG <ajaimezg@hotmail.com>
2017-10-19 20:27:12 +0200

    Commit inicial

---
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist1/Shoppinglistactivity.java
@@ -0,0 +1,23 @@
+package edu.upc.eseiaat.pma.shoppinglist1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class Shoppinglistactivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoppinglistactivity);
+
+
+    }
+}
