61f15495f73c44d4614059ffa3f73a02d2294e41
AlejandroJG <ajaimezg@hotmail.com>
2017-10-19 23:13:17 +0200

    Commit aplicació acabada

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist1/Shoppinglistactivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist1/Shoppinglistactivity.java
@@ -1,10 +1,13 @@
 package edu.upc.eseiaat.pma.shoppinglist1;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -59,6 +62,33 @@ public class Shoppinglistactivity extends AppCompatActivity {
         });
 
         list.setAdapter(adapter);//con este adapter meteremos lo que hay en el arraylist(itemlist) en la lista(list)
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            //creamos un listener en la misma lista para cuando cliquemos durante unos segundos
+            @Override
+            public boolean onItemLongClick(AdapterView<?> list, View item, int pos, long id) {
+                maybeRemoveItem(pos); //creamos un metodo para borrar un elemento de la lista i pasamos la posicion para saber quien el que borro
+                return true;
+            }
+        });
+    }
+
+
+
+    private void maybeRemoveItem(final int pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);//creamos un cuadro de dialogo
+        builder.setTitle(R.string.confirm); //Le ponemos un titulo a través de un recurso string a la confirmación de borrado
+        String fmt = getResources().getString(R.string.confirmmessage);
+        builder.setMessage(String.format(fmt, itemlist.get(pos))); //hacemos que salga el mensaje junto al elemento que queremos borrar con un format pasandole el pos de ese elemento
+        builder.setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        itemlist.remove(pos); //borramos ese elemento en el que hemos hecho un click largo
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, null); //si le damos al boton de cancel pues vuelve hacia atras(cancela)
+        builder.create().show(); //para que funcione el dialogo
 
 
     }
