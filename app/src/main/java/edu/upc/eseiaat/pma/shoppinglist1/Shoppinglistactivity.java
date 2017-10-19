737c18dc9ccca1be0791c5574960a19627b0b372
AlejandroJG <ajaimezg@hotmail.com>
2017-10-19 20:35:43 +0200

    Commit creació elements

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist1/Shoppinglistactivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist1/Shoppinglistactivity.java
@@ -12,11 +12,19 @@ import java.util.ArrayList;
 
 public class Shoppinglistactivity extends AppCompatActivity {
 
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoppinglistactivity);
+        list = (ListView) findViewById(R.id.list);//Con control+alt+f los convierto en campos
+        btn_add = (Button) findViewById(R.id.btn_añadir);
+        edit_item = (EditText) findViewById(R.id.edititem);
 
 
     }
